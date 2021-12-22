9b875765b164d061349d44d2863a7d8c561f17de
matsuura <matsuura@jsk.imi.i.u-tokyo.ac.jp>
2021-12-22 14:01:23 +0900

    add bat files

---
+++ scripts/win_roscore.bat
@@ -0,0 +1,5 @@
+rem @echo off
+rem c:\ros\catkin_ws\src\matsuura_hand\scripts\win_setup.bat
+echo "execute roscore"
+echo "please execute win_manus_ros_node.bat in other shell"
+rem roscore
---
+++ scripts/win_setup.bat
@@ -0,0 +1,6 @@
+@echo off
+rem c:\ros\catkin_ws\devel\setup.bat
+set ROS_MASTER_URI=http://192.168.97.88:11311
+set ROS_IP=192.168.97.176
+echo "set ROS_MASTER_URI : %ROS_MASTER_URI%"
+echo "set ROS_IP : %ROS_IP%"
